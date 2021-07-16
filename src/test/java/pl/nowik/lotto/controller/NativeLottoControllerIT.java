f83d9cbeb3ed5505a1d53cf375ab6480ba008f93
Piotr Nowicki <piotrowicki@gmail.com>
2021-07-16 13:12:25 +0200

    Initial commit

---
+++ src/test/java/pl/nowik/lotto/controller/NativeLottoControllerIT.java
@@ -0,0 +1,9 @@
+package pl.nowik.lotto.controller;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeLottoControllerIT extends LottoControllerTest {
+
+    // Execute the same tests but in native mode.
+}
