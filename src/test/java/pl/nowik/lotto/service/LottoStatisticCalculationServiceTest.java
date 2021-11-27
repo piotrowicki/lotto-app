c910e309581ac2109e42ba21c86f1ff75923be8b
Piotr Nowicki <piotrowicki@gmail.com>
2021-11-27 10:21:31 +0100

    Add calc test

---
+++ src/test/java/pl/nowik/lotto/service/LottoStatisticCalculationServiceTest.java
@@ -0,0 +1,67 @@
+package pl.nowik.lotto.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import pl.nowik.lotto.dto.LottoStatisticDto;
+import pl.nowik.lotto.repository.LottoRepository;
+import pl.nowik.lotto.util.LottoNumbersCollector;
+
+@QuarkusTest
+public class LottoStatisticCalculationServiceTest {
+
+    @Inject
+    LottoStatisticCalculationService service;
+
+    @InjectMock
+    LottoRepository repository;
+
+    @InjectMock
+    LottoNumbersCollector collector;
+
+    @BeforeEach
+    public void init() {
+        PanacheQuery panacheQuery = mock(PanacheQuery.class);
+        when(repository.findAll()).thenReturn(panacheQuery);
+    }
+
+    @Test
+    public void shouldGroupByQuantity() {
+        // when
+        List<Integer> draw = List.of(1, 2, 3, 3, 4, 5);
+
+        // when
+        when(collector.collectNumbersList(anyList())).thenReturn(draw);
+
+        List<LottoStatisticDto> result = service.calculateStats();
+
+        // then
+        assertNotNull(result);
+        assertResult(result, "1", "1");
+        assertResult(result, "2", "1");
+        assertResult(result, "3", "2");
+        assertResult(result, "4", "1");
+        assertResult(result, "5", "1");
+    }
+
+    private void assertResult(List<LottoStatisticDto> list, String number, String quantity) {
+        list.forEach(dto -> {
+            if (dto.getNumber().equals(number)) {
+                assertEquals(dto.getQuantity(), quantity);
+            }
+        });
+    }
+}
