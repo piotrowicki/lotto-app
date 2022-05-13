41af54e5691f8030a5c93b2a0d2fb094b72c569b
Piotr Nowicki <piotrowicki@gmail.com>
2022-05-13 20:07:43 +0200

    Remove native test

--- src/test/java/pl/nowik/lotto/controller/NativeLottoControllerIT.java
+++
@@ -1,9 +0,0 @@
-package pl.nowik.lotto.controller;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeLottoControllerIT extends LottoControllerTest {
-
-    // Execute the same tests but in native mode.
-}
